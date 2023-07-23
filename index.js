505d2dfe62e422091144f216f675f317e70f82db
karol.majerczyk <karol.majerczyk@gmail.com>
2023-07-23 12:36:15 +0200

    Created HTML structure, added CSS styles and JS functionality.

---
+++ index.js
@@ -0,0 +1,78 @@
+const posts = [
+  {
+    name: "Vincent van Gogh",
+    username: "vincey1853",
+    location: "Zundert, Netherlands",
+    avatar: "images/avatar-vangogh.jpg",
+    post: "images/post-vangogh.jpg",
+    comment: "just took a few mushrooms lol",
+    likes: 21,
+  },
+  {
+    name: "Gustave Courbet",
+    username: "gus1819",
+    location: "Ornans, France",
+    avatar: "images/avatar-courbet.jpg",
+    post: "images/post-courbet.jpg",
+    comment: "i'm feelin a bit stressed tbh",
+    likes: 4,
+  },
+  {
+    name: "Joseph Ducreux",
+    username: "jd1735",
+    location: "Paris, France",
+    avatar: "images/avatar-ducreux.jpg",
+    post: "images/post-ducreux.jpg",
+    comment:
+      "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+    likes: 152,
+  },
+];
+
+const postsEl = document.querySelector(".posts");
+
+postsEl.addEventListener("dblclick", function (e) {
+  const likes = e.target.querySelector(".current-likes");
+  let currentLikes = Number(likes.textContent);
+  likes.textContent = currentLikes + 1;
+});
+
+window.addEventListener("load", function () {
+  let html = "";
+
+  for (let i = 0; i < posts.length; i++) {
+    html += createPostEl(posts[i]);
+  }
+
+  postsEl.innerHTML = html;
+});
+
+function createPostEl(post) {
+  const postEl = `
+    <article class="post">
+      <div class="post-creator">
+        <img class="avatar" src="${post.avatar}" alt="" />
+        <div>
+          <p class="text-bold">${post.name}</p>
+          <p class="text-small">${post.location}</p>
+        </div>
+      </div>
+      <img src="${post.post}" alt="" />
+      <div class="post-details">
+        <div class="post-icons">
+          <img src="images/icon-heart.png" alt="" />
+          <img src="images/icon-comment.png" alt="" />
+          <img src="images/icon-dm.png" alt="" />
+        </div>
+        <p class="text-bold">
+          <span class="current-likes">${post.likes}</span> likes
+        </p>
+        <p>
+          <span class="text-bold">${post.username}</span> ${post.comment}
+        </p>
+      </div>
+    </article>
+  `;
+
+  return postEl;
+}
